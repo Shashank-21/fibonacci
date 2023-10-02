95810d6e416ea9fa47cfe647d23f2e8976532ce6
Shashank-21 <shashank@excelirate.net>
2023-10-02 17:37:23 +0530

    Chagne Fib component

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
             Fib Calculator
           </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">The Other Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
